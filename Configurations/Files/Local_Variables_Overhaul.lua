640e54d14503b04b47410ff86ee4569dd6a22760
Nyxarth <138682002+Nyxarth910@users.noreply.github.com>
2025-07-23 07:00:48 +0800

    Update Local_Variables_Overhaul.lua

--- Configurations/Files/Local_Variables_Overhaul.lua
+++ Configurations/Files/Local_Variables_Overhaul.lua
@@ -43,11 +43,21 @@ local Configuration = {
   PlayerAdjustmentType = "Optimized"
   
   DefaultPlayerAdjustment = {
-    Gatekeeped = true
+    Speed = 1500,
+    JumpHeight = 3,
+    JumpCap = 1,
+    JumpAcceleration = 1.5,
+    AirStrafe = 182,
+    GroundAcceleration = 5
   }
 
   OverridePlayerAdjustment = {
-    Gatekeeped = true
+    Speed = 1500,
+    JumpHeight = 3,
+    JumpCap = 1,
+    JumpAcceleration = 1.5,
+    AirStrafe = 182,
+    GroundAcceleration = 5
   }
 
   Humanoids = {
@@ -81,5 +91,27 @@ local Configuration = {
     SelectedEmote = "BoldMarch",
   }
 
-  
+  MovementModification = {
+    FakeEmoteDash = false,
+    EmoteSpeed = 2500,
+    InfiniteSlide = false,
+    SlideAcceleration = -3,
+    BHOPType = "Acceleration",
+    BHOPAcceleration = -0.1,
+    BHOPEnable = false,
+    DisableBHOPUncap = false,
+    HipHeight1 = 0,
+    HipHeight2 = 0,
+    LagSwitchEnable = false,
+    LagSwitchFPSCap = "1"
+    LagSwitchDelay = 0.1
+  }
+
+  AntiLags = {
+    Low = false,
+    Moderate = false,
+    High = false
+  }
+
+  Visual = {}
 }
