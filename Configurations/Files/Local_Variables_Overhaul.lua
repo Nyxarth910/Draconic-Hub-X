53450e5b514f24d04bd7898d3d5fecd09a2f39a7
Nyxarth <138682002+Nyxarth910@users.noreply.github.com>
2025-07-22 23:31:09 +0800

    Update Local_Variables_Overhaul.lua

--- Configurations/Files/Local_Variables_Overhaul.lua
+++ Configurations/Files/Local_Variables_Overhaul.lua
@@ -1,1 +1,85 @@
 
+-- Too lazy, I'll finish it later.
+
+local Configuration = {
+  ESP = {
+    Players = false,
+    Nextbots = false,
+    Tickets = false,
+    Objective = false
+  }
+
+  Tracers = {
+    Players = false,
+    Nextbots = false,
+    Tickets = false,
+    Objective = false
+  }
+
+  Removalist = {
+    CameraShake = false,
+    ReducingRewards = false,
+    DamageParts = false
+  }
+
+  ShowTimer = false,
+  Fly = false,
+  FlySpeed = 20,
+  Noclip = false,
+
+  FarmingStates = {
+    IsReviving = false,
+    IsCollectingTickets = false
+  }
+
+  AFKFarm = false,
+  FarmTickets = false,
+  FarmTokens = false,
+
+  AntiNextbot = false,
+  AntiNextbotRange = 15,
+  AntiNextbotType = "Spawn",
+
+  PlayerAdjustmentType = "Optimized"
+  
+  DefaultPlayerAdjustment = {
+    Gatekeeped = true
+  }
+
+  OverridePlayerAdjustment = {
+    Gatekeeped = true
+  }
+
+  Humanoids = {
+    WalkspeedCF = false,
+    OriginalJumpHeight = false,    
+    CF = 5,
+    JP = 20
+  }
+
+  BounceAdjustment = {
+    GetCurrentSpeed = 0,
+    EnableBounce = false,
+    DefaultBounce = 80,
+    EmoteBounce = 120
+  }
+
+  CameraAdjustment = {
+    StretchX = 1,
+    StretchY = 1
+  }
+
+  GunAdjustment = {
+    v = nil
+  }
+
+  GameAutomation = {
+    InstantRevive = false,
+    ReviveDelay = 0.1,
+    AutoCarry = false,
+    MacroMode = false,
+    SelectedEmote = "BoldMarch",
+  }
+
+  
+}
