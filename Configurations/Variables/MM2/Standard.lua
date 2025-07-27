07e708a924aefebc17d4e1aa1be1883f7e41f2ba
Nyxarth <138682002+Nyxarth910@users.noreply.github.com>
2025-07-27 15:15:49 +0800

    Update and rename Standard to Standard.lua

---
+++ Configurations/Variables/MM2/Standard.lua
@@ -0,0 +1,31 @@
+local DConfiguration = {
+  ESP = {
+    Innocent = false,
+    Sheriff = false,
+    Murderer = false,
+  },
+
+  Tracers = {
+    Innocent = false,
+    Sheriff = false,
+    Murderer = false,
+  },
+
+  Boxes = {
+    Innocent = false,
+    Sheriff = false,
+    Murderer = false,
+  },
+  
+  SpoofRole = {
+    Murderer = false,
+    Sheriff = false,
+    Perks = false,
+    Yourself = false,
+  },
+
+  
+  
+}
+
+return DConfiguration
