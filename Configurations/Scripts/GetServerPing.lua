9ab65bd59113820f6bf2ea5f6a9c5acda785a106
Vyznixwasnotfound <vyznian@gmail.com>
2025-08-11 10:22:50 +0800

    Update GetServerPing.lua

--- Configurations/Scripts/GetServerPing.lua
+++ Configurations/Scripts/GetServerPing.lua
@@ -1,1 +1,7 @@
+getgenv().CurrentServerPing = 0
 
+spawn(function()
+    while task.wait() do      
+      getgenv().CurrentServerPing = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue()
+    end
+end)
