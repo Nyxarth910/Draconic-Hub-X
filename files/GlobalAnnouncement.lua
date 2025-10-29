5d2bdeeb0db755d0a584193b29af0cd4bac97ff4
Vyznixwasnotfound <vyznian@gmail.com>
2025-10-29 16:53:03 +0800

    Fix typo in GlobalAnnouncement content

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = "Did you know jax is the tuffest of our Discord server 🗿",
+   Content = "Did you know? jax is the tuffest of our Discord server 🗿",
    SubContent = "", -- Optional
    Duration = 15
 })
