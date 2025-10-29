e06a5be8f5512d412ee7724f257d8734d70e2ced
Vyznixwasnotfound <vyznian@gmail.com>
2025-10-29 16:52:30 +0800

    Update GlobalAnnouncement.lua

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,7 +1,6 @@
-
 Fluent:Notify({
-   Title = "Test Announcement",
-   Content = "!!!",
+   Title = "Aerave Message's",
+   Content = "Did you know jax is the tuffest of our Discord server 🗿",
    SubContent = "", -- Optional
    Duration = 15
 })
