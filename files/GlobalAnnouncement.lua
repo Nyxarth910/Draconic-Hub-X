f09c4b1dc12e99e8584166c27587cffc5565158b
Aerave <vyznian@gmail.com>
2025-10-30 11:20:03 +0800

    Update GlobalAnnouncement.lua

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,12 +1,5 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = "Did you know? jax is the tuffest of our Discord server 🗿",
-   SubContent = "", -- Optional
-   Duration = 15
-})
-
-Fluent:Notify({
-   Title = "Aerave Message's [2]",
    Content = "Did you know? Unknown Hub X was renamed to Draconic Hub X?",
    SubContent = "", -- Optional
    Duration = 15
