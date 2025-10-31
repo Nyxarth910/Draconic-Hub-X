779daa4e8341b78180a17e463ec4f68bb87b3162
Aerave <vyznian@gmail.com>
2025-10-31 10:27:22 +0800

    Fix typo in GlobalAnnouncement.lua content

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = "Did you know? Keyless Mode Actives every Saturday to Monday?",
+   Content = "Did you know? Keyless Mode Activates every Saturday to Monday?",
    SubContent = "", -- Optional
    Duration = 15
 })
