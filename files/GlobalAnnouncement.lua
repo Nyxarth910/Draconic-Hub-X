a9a91348f6c836dc4facc51c0664c6fef49c869e
Aerave <vyznian@gmail.com>
2025-11-16 14:46:07 +0800

    Update GlobalAnnouncement with new subcontent

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
    Content = "oi im not dead yet >:(, im just tired",
-   SubContent = "", -- Optional
+   SubContent = "Also my discord account was hacked", -- Optional
    Duration = 15
 })
