71ac51fc990074cf12e9d6236f89c8acae397fe4
Aerave <vyznian@gmail.com>
2025-11-19 21:11:58 +0800

    Update GlobalAnnouncement.lua

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = ":thinking_pufferfish:",
+   Content = "Visual is still broken :mad_cat::mad_cat::mad_cat:",
    SubContent = "", -- Optional
    Duration = 15
 })
