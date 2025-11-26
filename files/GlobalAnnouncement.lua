ad7d16b7a63f43c14f598395699609285b266036
Aerave <vyznian@gmail.com>
2025-11-26 17:25:18 +0800

    Add notification for Visual release
    Added a notification for the release of Visual.

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,1 +1,6 @@
-
+Fluent:Notify({
+   Title = "Aerave Message's",
+   Content = "Visual is released now :very_suspicious_kitty:, and stop arguing me for removing the visual for no reason",
+   SubContent = "", -- Optional
+   Duration = 15
+})
