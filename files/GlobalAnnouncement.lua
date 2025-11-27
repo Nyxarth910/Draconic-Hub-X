e5ccc8da2f13499e0d92cb96fee3673ea0192874
Aerave <vyznian@gmail.com>
2025-11-27 23:15:12 +0800

    Update GlobalAnnouncement content message

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = "Visual is released now :very_suspicious_kitty:, and stop arguing me for removing the visual for no reason",
+   Content = ""dragonic" 🤦",
    SubContent = "", -- Optional
    Duration = 15
 })
