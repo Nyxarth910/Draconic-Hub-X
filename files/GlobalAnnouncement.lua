3cef4e653cf103a9c8d4ebeac1059f4fd99e4eb1
Aerave <vyznian@gmail.com>
2025-11-29 18:24:46 +0800

    Update GlobalAnnouncement content message

--- files/GlobalAnnouncement.lua
+++ files/GlobalAnnouncement.lua
@@ -1,6 +1,6 @@
 Fluent:Notify({
    Title = "Aerave Message's",
-   Content = "'dragonic' 🤦",
+   Content = "there's a secret update :p (NOT THE AUTO CATCH TURKEY)",
    SubContent = "", -- Optional
    Duration = 15
 })
