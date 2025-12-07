88ccdafbcf7fd2e1093ed9a046b8fc09e68ac516
Pnsdgsa <hfjthyhjtjyfbrmyghfhgh@gmail.com>
2025-12-07 11:16:29 +0700

    Update Floating Button.lua

--- File/Floating Button.lua
+++ File/Floating Button.lua
@@ -14,20 +14,6 @@ button.Position = UDim2.new(0.5, -75, 0.5, -75)
 button.BackgroundTransparency = 1
 button.Parent = gui
 
-local backgroundImage = Instance.new("ImageLabel")
-backgroundImage.Name = "SpinningBackground"
-backgroundImage.Size = UDim2.new(1, 0, 1, 0)
-backgroundImage.AnchorPoint = Vector2.new(0.5, 0.5)
-backgroundImage.Position = UDim2.new(0.5, 0, 0.5, 0)
-backgroundImage.BackgroundTransparency = 1
-backgroundImage.Image = "rbxassetid://88077369114691"
-backgroundImage.ZIndex = 1
-backgroundImage.Parent = button
-
-local backgroundScale = Instance.new("UIScale")
-backgroundScale.Scale = 1.5
-backgroundScale.Parent = backgroundImage
-
 local topImage = Instance.new("ImageButton")
 topImage.Name = "TopAsset"
 topImage.Size = UDim2.new(0.7, 0, 0.7, 0)
@@ -37,6 +23,22 @@ topImage.BackgroundTransparency = 1
 topImage.Image = "rbxassetid://108180785726591"
 topImage.ZIndex = 2
 topImage.Parent = button
+topImage.Draggable = true
+
+local backgroundImage = Instance.new("ImageLabel")
+backgroundImage.Name = "SpinningBackground"
+backgroundImage.Size = UDim2.new(1, 0, 1, 0)
+backgroundImage.AnchorPoint = Vector2.new(0.5, 0.5)
+backgroundImage.Position = UDim2.new(0.5, 0, 0.5, 0)
+backgroundImage.BackgroundTransparency = 1
+backgroundImage.Image = "rbxassetid://88077369114691"
+backgroundImage.ZIndex = 1
+backgroundImage.Parent = topImage
+backgroundImage.Interactable = false 
+
+local backgroundScale = Instance.new("UIScale")
+backgroundScale.Scale = 2
+backgroundScale.Parent = backgroundImage
 
 local corner = Instance.new("UICorner")
 corner.CornerRadius = UDim.new(1, 0)
@@ -83,4 +85,12 @@ end)
 
 topImage.MouseButton1Click:Connect(function()
 	print("Round button clicked!")
-end) 
+	local audioUrl = "https://github.com/010101010101010111/010101010101010111-010101010101010111-1001100101100101001001101010101010101101001010101010101101010/raw/refs/heads/main/File/audio/Bruh%20sound%20effect_256k.mp3"
+local request = http_request or (syn and syn.request) or request
+local response = request({Url = audioUrl, Method = "GET"})
+writefile("bruh.mp3", response.Body)
+local sound = Instance.new("Sound")
+sound.SoundId = getcustomasset("bruh.mp3")
+sound.Parent = game:GetService("SoundService")
+sound:Play()
+end)
